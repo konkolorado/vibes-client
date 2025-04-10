777d80357424ea721e984faeeb516354be167ba9
konkolorado <5422638+konkolorado@users.noreply.github.com.>
2025-04-10 00:11:58 -0500

    ft: finishes wallet item token update command

--- bin/wallet_campaign.rb
+++ bin/wallet_campaign.rb
@@ -38,14 +38,17 @@ class WalletCampaign < Thor
     puts JSON.pretty_generate(item.body)
   end
 
-  desc 'item_put COMPANY_KEY CAMPAIGN_ID ITEM_ID', "Display an item in company's wallet campaign"
-  def item_put(company_key, campaign_id, item_id)
+  desc 'item_update COMPANY_KEY CAMPAIGN_ID ITEM_ID [key:value, ...]', 'Set tokens on an item'
+  def item_update(company_key, campaign_id, item_id, *tokens)
+    tokens = tokens.each_with_object({}) do |pair, h|
+      key, value = pair.split(':', 2)
+      h[key] = value
+    end
+
     log_level = options[:verbose] ? :debug : :warn
     wc = WalletCampaignClient.new(company_key: company_key, log_level: log_level)
     item = wc.update_wallet_campaign_item_by_id(
-      campaign_id,
-      item_id,
-      { "tokens": { "expiration_date": '2017-02-18T19:30Z' } }
+      campaign_id, item_id, { "tokens": tokens }
     )
     puts JSON.pretty_generate(item.body)
   end

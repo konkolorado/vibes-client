15f5659a59cd7bbd4529009583820c71e6714f5c
Uriel Mandujano <5422638+konkolorado@users.noreply.github.com>
2025-04-10 23:44:34 -0500

    ft: adds more person finding commands

--- bin/person.rb
+++ bin/person.rb
@@ -2,11 +2,27 @@ require 'thor'
 require 'vibes_client'
 
 class Person < Thor
-  desc 'mdn ID', 'Display a person record'
+  desc 'mdn COMPANY_KEY ID', 'Get a person record by their MDN'
   def mdn(company_key, mdn)
     log_level = options[:verbose] ? :debug : :warn
     pc = PersonClient.new(company_key: company_key, log_level: log_level)
-    person = pc.find_by_mdn(mdn)
+    person = pc.get_by_mdn(mdn)
+    puts JSON.pretty_generate(person.body)
+  end
+
+  desc 'epid COMPANY_KEY ID', 'Get a person record by their external person ID'
+  def epid(company_key, mdn)
+    log_level = options[:verbose] ? :debug : :warn
+    pc = PersonClient.new(company_key: company_key, log_level: log_level)
+    person = pc.get_by_mdn(mdn)
+    puts JSON.pretty_generate(person.body)
+  end
+
+  desc 'pk COMPANY_KEY KEY', 'Get a person record by their person key'
+  def pk(company_key, mdn)
+    log_level = options[:verbose] ? :debug : :warn
+    pc = PersonClient.new(company_key: company_key, log_level: log_level)
+    person = pc.get_by_person_key(mdn)
     puts JSON.pretty_generate(person.body)
   end
 end
--- lib/vibes_client/person_client.rb
+++ lib/vibes_client/person_client.rb
@@ -8,7 +8,7 @@ class PersonClient
     @company_key = company_key
   end
 
-  def find_by_mdn(mdn)
+  def get_by_mdn(mdn)
     params = { mdn: mdn }
     endpoint = "/companies/#{@company_key}/mobiledb/persons"
     @client.get(endpoint, params)
@@ -16,6 +16,11 @@ class PersonClient
 
   def get_by_person_key(key)
     endpoint = "/companies/#{@company_key}/mobiledb/persons/#{key}"
+    @client.get(endpoint)
+  end
+
+  def get_by_external_person_id(epid)
+    endpoint = "companies/#{company_key}/mobiledb/persons/external/#{epid}"
     @client.get(endpoint)
   end
 end

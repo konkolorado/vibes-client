244a8c3e7cd9fc04f9fe46b8088dfe331a638678
konkolorado <5422638+konkolorado@users.noreply.github.com.>
2025-04-14 16:20:02 -0500

    ft: support add+remove external person ids

--- bin/person.rb
+++ bin/person.rb
@@ -27,4 +27,20 @@ class Person < Thor
     person = pc.get_by_person_key(mdn)
     puts JSON.pretty_generate(person.body)
   end
+
+  desc 'clear-epid COMPANY_KEY PERSON_KEY', "Remove a person's external person id"
+  def clear_epid(company_key, person_key)
+    log_level = options[:verbose] ? :debug : :warn
+    pc = PersonClient.new(company_key: company_key, log_level: log_level)
+    person = pc.remove_external_person_id_by_person_key(person_key)
+    puts JSON.pretty_generate(person.body)
+  end
+
+  desc 'set-epid COMPANY_KEY PERSON_KEY EXTERNAL_PERSON_ID', "Set a person's external person id"
+  def set_epid(company_key, person_key, external_person_id)
+    log_level = options[:verbose] ? :debug : :warn
+    pc = PersonClient.new(company_key: company_key, log_level: log_level)
+    person = pc.add_external_person_id_by_person_key(person_key, external_person_id)
+    puts JSON.pretty_generate(person.body)
+  end
 end
--- lib/vibes_client/person_client.rb
+++ lib/vibes_client/person_client.rb
@@ -25,4 +25,14 @@ class PersonClient
     endpoint = "companies/#{company_key}/mobiledb/persons/external/#{epid}"
     @client.get(endpoint)
   end
+
+  def remove_external_person_id_by_person_key(key)
+    endpoint = "/companies/#{@company_key}/mobiledb/persons/#{key}"
+    @client.put(endpoint, { external_person_id: nil })
+  end
+
+  def add_external_person_id_by_person_key(key, external_person_id)
+    endpoint = "/companies/#{@company_key}/mobiledb/persons/#{key}"
+    @client.put(endpoint, { external_person_id: external_person_id })
+  end
 end
